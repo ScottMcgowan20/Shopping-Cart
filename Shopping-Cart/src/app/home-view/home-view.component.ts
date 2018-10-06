3c3eec76dd114db446236af8623ef0490f6b3001
scooterpooter24 <scoott@vt.edu>
2018-10-06 11:07:57 -0400

    Beginning to make different options for store

---
+++ Shopping-Cart/src/app/home-view/home-view.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-view',
+  templateUrl: './home-view.component.html',
+  styleUrls: ['./home-view.component.css']
+})
+export class HomeViewComponent implements OnInit {
+
+  numShirts;
+
+  constructor() {
+    this.numShirts = 0;
+   }
+
+  ngOnInit() {
+  }
+
+}
